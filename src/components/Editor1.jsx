4f91c089c46f4da588e208acdfb9afed2fa3c14e
Abhishek Gupta <ambitiousabhishek7@gmail.com>
2022-06-28 16:06:54 +0530

    gridAdjusted

--- src/components/Editor1.jsx
+++ src/components/Editor1.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
 import Twogrid from "./Twogrid";
-import Threegrid from "./Threegrid"
+import Threegrid from "./Threegrid";
 import SplitPane from "react-split-pane";
 import Split from "react-split";
-let option1="horizontal";
-let option2="vertical";
+let option1 = "horizontal";
+let option2 = "vertical";
+// let defaultsize = "600px";
 export const Editor1 = (props) => {
   return (
     <div>
@@ -14,69 +15,66 @@ export const Editor1 = (props) => {
        <Twogrid option={option2} /> 
       }*/}
       {/* threegrids option */}
-      {/* {
-         <Threegrid option={option2} /> 
-      }  */}
+      {/* {<Threegrid size="600px" option={option2} />} */}
       {/* props==4 */}
-     {/* <SplitPane split={option2}  defaultSize="300px" minSize={-1} maxSize="33.3%">
-     <Twogrid option={option2} /> 
-     <Twogrid option={option1} /> 
+      {/* <SplitPane split={option1}  defaultSize="300px" minSize={-1} maxSize="33.3%">
+     <Twogrid size="50%" option={option2} /> 
+     <Twogrid size="50%" option={option2} /> 
       </SplitPane> */}
 
       {/* 5 count */}
       {/* <SplitPane split={option1}  defaultSize="300px" minSize={-1} maxSize="33.3%">
-      <Twogrid option={option2} /> 
-      <Threegrid option={option2} />
-      </SplitPane> */}
-      {/* <SplitPane split={option1}  defaultSize="300px" minSize={-1} maxSize="33.3%">
-      <Threegrid option={option2} />
-      <Threegrid option={option2} />
+      <Twogrid size="50%"option={option2} /> 
+      <Threegrid size="550px" option={option2} />
       </SplitPane> */}
 
-{/* 7 count */}
-{/* <SplitPane split="horizontal">
-<Threegrid/>
-      <SplitPane>
-        <div><Twogrid/></div>
-        <div> <Twogrid/></div>
+      {/* 6 count */}
+      {/* <SplitPane split={option1}  defaultSize="350px" minSize={-1} maxSize="33.3%">
+      <Threegrid size="550px" option={option2} />
+      <Threegrid size="550px" option={option2} />
+      </SplitPane> */}
+ 
+      {/* 7 count */}
+      {/* <SplitPane split={option1}  defaultSize="350px" minSize={-1} maxSize="33.3%">
+      <Threegrid size="550px" option={option2}/>
+      <SplitPane defaultSize="50%" >
+        <div><Twogrid size="50%" option={option2}/></div>
+        <div> <Twogrid size="50%" option={option2}/></div>
       </SplitPane>
       </SplitPane> */}
 
-{/* 8 count */}
-{/* <SplitPane split="horizontal">
-
-  <SplitPane>
-    <div><Twogrid/></div>
-    <div><Twogrid/></div>
-  </SplitPane>
-  <SplitPane>
-    <div><Twogrid/></div>
-    <div><Twogrid/></div>
-  </SplitPane>
-</SplitPane> */}
-
+      {/* 8 count */}
+      {/* <SplitPane split={option1} defaultSize="50%">
+          <SplitPane defaultSize="50%">
+            <div><Twogrid size="50%" option={option2}/></div>
+            <div><Twogrid size="50%" option={option2}/></div>
+          </SplitPane>
+          <SplitPane defaultSize="50%">
+            <div><Twogrid size="50%" option={option2}/></div>
+            <div><Twogrid size="50%" option={option2}/></div>
+          </SplitPane>
+      </SplitPane> */}
 
       {/* 9 count */}
-      {/* <SplitPane split="horizontal">
-        <Threegrid/>
-        <SplitPane split="horizontal">
-          <Threegrid/>
-          <Threegrid/>
+      {/* <SplitPane split="horizontal" defaultSize="33.3%">
+        <Threegrid size="40.0%"/>
+        <SplitPane split="horizontal" defaultSize="50.0%">
+          <Threegrid size="40.0%"/>
+          <Threegrid size="40.0%"/>
         </SplitPane>
       </SplitPane> */}
 
       {/* 10 count */}
-<SplitPane split="horizontal">
-
-<SplitPane>
-    <div><Twogrid/></div>
-    <div><Threegrid/></div>
-  </SplitPane>
-  <SplitPane><div><Twogrid/></div>
-    <div><Threegrid/></div></SplitPane>
-</SplitPane>
-           
-
+      <SplitPane split="horizontal" defaultSize="50.0%">
+        <SplitPane split={option2} defaultSize="40.0%">
+            <div><Twogrid option={option2}size="40.0%"/></div>
+            <div><Threegrid option={option2}size="40.0%"/></div>
+          </SplitPane>
+          <SplitPane split={option2} defaultSize="40.0%">
+            <div><Twogrid option={option2}size="40.0%"/></div>
+            <div><Threegrid option={option2}size="40.0%"/></div>
+          </SplitPane>
+      </SplitPane>
     </div>
   );
 };
