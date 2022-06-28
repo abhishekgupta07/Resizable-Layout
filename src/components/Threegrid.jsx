44ff1996fc45edbbb79db232b7149f9e2ea074fc
Abhishek Gupta <ambitiousabhishek7@gmail.com>
2022-06-28 13:11:29 +0530

    7grid layout added

--- src/components/Threegrid.jsx
+++ src/components/Threegrid.jsx
@@ -4,7 +4,7 @@ import logo from "./logoo.png";
 function Threegrid({ option }) {
     return (
         <div>
-            <SplitPane split={option} defaultSize="200px" minSize={0} maxSize={-1520} primary='first' >
+            <SplitPane split={option} defaultSize="200px" minSize={0} maxSize="100%" primary='first' >
                 <div style={{ width: "100%", height: "100%", backgroundColor: "#484848", }}>layout2 <img src={logo} alt="logo" width="auto" height="auto" /></div>
                 <SplitPane split={option} defaultSize="500px" minSize={0} maxSize="calc(1700px-1000px)" primary='second' >
                     <div style={{ width: "100%", height: "100%", backgroundColor: "#484848", }}>layout2 <img src={logo} alt="logo" width="auto" height="auto" /></div>
