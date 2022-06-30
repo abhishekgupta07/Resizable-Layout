c4d0b2e76dbfa16e43d065cf598a02c044b4a453
Utkarshking <Utkarshsinghclash@gmail.com>
2022-06-30 12:51:56 +0530

    added variables till 7

--- src/components/Threegrid.jsx
+++ src/components/Threegrid.jsx
@@ -3,19 +3,18 @@ import SplitPane from "react-split-pane";
 import logo from "./logoo.png";
 import Twogrid from './Twogrid';
 let secondpane = "550px";
-function Threegrid({ option, size,defaultSizepara2, onChangehandler2 }) {
+function Threegrid({ option, size, defaultSizepara2, onChangehandler2, defaultSizepara, onChangehandler }) {
     return (
         <div>
             <SplitPane split={option} defaultSize={defaultSizepara2} minSize={10} maxSize={secondpane} primary='first' onChange={onChangehandler2} >
                 <div style={{ width: "100%", height: "100%", backgroundColor: "#484848", }}>layout2 <img src={logo} alt="logo" width="auto" height="auto" /></div>
-                {/* <SplitPane split={option} defaultSize={secondpane} minSize={8} maxSize={-5} primary='second' >
+                {/* <SplitPane split={option} defaultSize={defaultSizepara} minSize={8} maxSize={-5} primary='second' onChange={onChangehandler} >
                     <div style={{ width: "auto", height: "100%", backgroundColor: "#484848", }}>layout2 <img src={logo} alt="logo" width="auto" height="auto" /></div>
                     <div style={{ width: "100%", height: "100%", backgroundColor: "#484848", }}>layout2 <img src={logo} alt="logo" width="auto" height="auto" /></div>
                 </SplitPane> */}
-
                 <Twogrid option={option} 
-                defaultSizepara={parseInt(localStorage.getItem('2v2'), 10)}
-                onChangehandler={(size)=>{localStorage.setItem('2v2',size)}}
+                defaultSizepara={defaultSizepara}
+                onChangehandler={onChangehandler}
                  />
             </SplitPane>
         </div >
