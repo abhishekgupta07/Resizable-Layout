4d88f6c4dab8d576f6c28271318ac3d8232c1174
Abhishek Gupta <ambitiousabhishek7@gmail.com>
2022-07-04 14:38:08 +0530

    container and functions added for looping

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import Editor1 from './components/Editor1';
+import "./App.css";
+import Editor1 from "./components/Editor1";
+import Flexboxbuttons from "./components/Flexboxbuttons";
 // import Resizablelayout from './components/Resizablelayout';
 
 function App() {
-  
   return (
-    <div className="App">
-     {/* <Resizablelayout/> */}
-     {/* <resizableLayoutPackage/> */}
-     <Editor1 />
+    <div className="App" >
+      
+        <Editor1 />
+
+      {/* </div> */}
     </div>
   );
 }
