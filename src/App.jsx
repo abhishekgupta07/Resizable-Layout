a287c15d57290cb1fb2aed461eab6a8e9aea7497
Utkarshking <Utkarshsinghclash@gmail.com>
2022-07-04 15:17:12 +0530

    state variables added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Editor1 from "./components/Editor1";
 import Flexboxbuttons from "./components/Flexboxbuttons";
 // import Resizablelayout from './components/Resizablelayout';
-import Flexboxbuttons from './components/Flexboxbuttons';
+// import Flexboxbuttons from './components/Flexboxbuttons';
 function App() {
   return (
     <div className="App">
