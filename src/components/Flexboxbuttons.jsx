4ff6c5ca128a7d099def1d1a573d0198a684553c
Utkarshking <Utkarshsinghclash@gmail.com>
2022-07-04 17:22:38 +0530

    the state variables changed on page refresh

--- src/components/Flexboxbuttons.jsx
+++ src/components/Flexboxbuttons.jsx
@@ -3,124 +3,171 @@ import React, { Component } from "react";
 import "./flexbox.scss";
 import Editor1 from "./Editor1";
 export class Flexboxbuttons extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gridnumber: 4,
-    };
-  }
-  for1 = () => {
-    this.setState({
-      gridnumber: 1,
-    });
-    // alert("grid activated",this.state.gridnumber)
-  };
-  for2 = () => {
-    this.setState({
-      gridnumber: 2,
-    });
-    // alert("grid activated",this.state.gridnumber)
-  };
-  for3 = () => {
-    this.setState({
-      gridnumber: 3,
-    });
-    // alert("grid activated",this.state.gridnumber)
-  };
-  for4 = () => {
-    this.setState({
-      gridnumber: 4,
-    });
-    // alert(this.state.gridnumber)
-  };
-  for5 = () => {
-    this.setState({
-      gridnumber: 5,
-    });
-    // alert(this.state.gridnumber)
-  };
-  for6 = () => {
-    this.setState({
-      gridnumber: 6,
-    });
-    // alert("grid activated",this.state.gridnumber)
-  };
-  render() {
-    // alert(this.state.gridnumber);
-    console.log(this.state.gridnumber);
-    return (
-      <div>
-        <div className="container">
-          <div className="parent-container-flex">
-            <button
-              type="button"
-              class="parent-flex-item"
-              id="button-1"
-              onClick={this.for1}
-            >
-              <div className="child-container-flex">
-                <div class="child-flex-item1" id="flex-child"></div>
-              </div>
-            </button>
-            <button class="parent-flex-item" id="button-2" onClick={this.for2}>
-              <div className="child-container-flex">
-                <div class="child-flex-item2" id="flex-child"></div>
-                <div class="child-flex-item2" id="flex-child"></div>
-              </div>
-            </button>
-            <button class="parent-flex-item" id="button-3" onClick={this.for3}>
-              <div className="child-container-flex">
-                <div class="child-flex-item-3" id="flex-child"></div>
-                <div class="child-flex-item-3" id="flex-child"></div>
-                <div class="child-flex-item-3" id="flex-child"></div>
-              </div>
-            </button>
-            <button class="parent-flex-item" id="button-5" onClick={this.for4}>
-              <div className="child-container-flex-4">
-                <div class="child-flex-item4">
-                  <div class="child-flex-item4-child"></div>
-                  <div class="child-flex-item4-child"></div>
+    constructor(props) {
+        super(props);
+        // console.log(localStorage.getItem('gridnumber'))
+        this.state = {
+            gridnumber: JSON.parse(localStorage.getItem('gridnumber')) || 2
+        };
+    }
+    // for1 = () => {
+    //     this.setState({
+    //         gridnumber: 1,
+    //     });
+    //     localStorage.setItem('gridnumber', 1)
+
+    //     // alert("grid activated",this.state.gridnumber)
+    // };
+    // for2 = () => {
+    //     this.setState({
+    //         gridnumber: 2,
+    //     });
+    //          localStorage.setItem('gridnumber', 2)
+    //     // alert("grid activated",this.state.gridnumber)
+    // };
+    // for3 = () => {
+    //     this.setState({
+    //         gridnumber: 3,
+    //     });
+    //     localStorage.setItem('gridnumber',3) 
+    //     // alert("grid activated",this.state.gridnumber)
+    // };
+    // for4 = () => {
+    //     this.setState({
+    //         gridnumber: 4,
+    //     });
+    //     localStorage.setItem('gridnumber',4) 
+    //     // alert(this.state.gridnumber)
+    // };
+    // for5 = () => {
+    //     this.setState({
+    //         gridnumber: 5,
+    //     });
+    //     localStorage.setItem('gridnumber',5) 
+    // };
+    // for6 = () => {
+    //     this.setState({
+    //         gridnumber: 6,
+    //     });
+    //     localStorage.setItem('gridnumber',6)
+    //         // this.setState({ gridnumber: localStorage.setItem('gridnumber', this.state.gridnumber) })
+             
+    //         // alert("grid activated",this.state.gridnumber)
+    // };
+    changestate=(gridnum)=>{
+        if(gridnum===1){
+            this.setState({
+                gridnumber: 1,
+            });
+            localStorage.setItem('gridnumber', 1)
+        }
+        if(gridnum===2){
+            this.setState({
+                gridnumber: 2,
+            });
+        localStorage.setItem('gridnumber', 2)
+        }
+        if(gridnum===3){
+            this.setState({
+                gridnumber: 3,
+            });
+            localStorage.setItem('gridnumber',3) 
+        }
+        if(gridnum===4){
+            this.setState({
+                gridnumber: 4,
+            });
+            localStorage.setItem('gridnumber',4) 
+        }
+        if(gridnum===5){
+            this.setState({
+                gridnumber: 5,
+            });
+            localStorage.setItem('gridnumber',5)
+        }
+        if(gridnum===6){
+            this.setState({
+                gridnumber: 6,
+            });
+            localStorage.setItem('gridnumber',6)
+        }
+
+    }
+    render() {
+        // alert(this.state.gridnumber);
+        console.log(this.state.gridnumber);
+        return (
+            <div>
+                <div className="container">
+                    <div className="parent-container-flex">
+                        <button
+                            type="button"
+                            class="parent-flex-item"
+                            id="button-1"
+                            onClick={this.for1}
+                        >
+                            <div className="child-container-flex">
+                                <div class="child-flex-item1" id="flex-child"></div>
+                            </div>
+                        </button>
+                        <button class="parent-flex-item" id="button-2" onClick={()=>this.changestate(2)}>
+                            <div className="child-container-flex">
+                                <div class="child-flex-item2" id="flex-child"></div>
+                                <div class="child-flex-item2" id="flex-child"></div>
+                            </div>
+                        </button>
+                        <button class="parent-flex-item" id="button-3" onClick={()=>this.changestate(3)}>
+                            <div className="child-container-flex">
+                                <div class="child-flex-item-3" id="flex-child"></div>
+                                <div class="child-flex-item-3" id="flex-child"></div>
+                                <div class="child-flex-item-3" id="flex-child"></div>
+                            </div>
+                        </button>
+                        <button class="parent-flex-item" id="button-5" onClick={()=>this.changestate(4)}>
+                            <div className="child-container-flex-4">
+                                <div class="child-flex-item4">
+                                    <div class="child-flex-item4-child"></div>
+                                    <div class="child-flex-item4-child"></div>
+                                </div>
+                                <div class="child-flex-item4">
+                                    <div class="child-flex-item5-child"></div>
+                                    <div class="child-flex-item5-child"></div>
+                                </div>
+                            </div>
+                        </button>
+                        <button class="parent-flex-item" id="button-5" onClick={()=>this.changestate(5)}>
+                            <div className="child-container-flex-4">
+                                <div class="child-flex-item4">
+                                    <div class="child-flex-item4-child"></div>
+                                    <div class="child-flex-item4-child"></div>
+                                </div>
+                                <div class="child-flex-item4">
+                                    <div class="child-flex-item5-child"></div>
+                                    <div class="child-flex-item5-child"></div>
+                                    <div class="child-flex-item5-child"></div>
+                                </div>
+                            </div>
+                        </button>
+                        <button class="parent-flex-item" id="button-6" onClick={()=>this.changestate(6)}>
+                            <div className="child-container-flex-4">
+                                <div class="child-flex-item4">
+                                    <div class="child-flex-item5-child"></div>
+                                    <div class="child-flex-item5-child"></div>
+                                    <div class="child-flex-item5-child"></div>
+                                </div>
+                                <div class="child-flex-item4">
+                                    <div class="child-flex-item5-child"></div>
+                                    <div class="child-flex-item5-child"></div>
+                                    <div class="child-flex-item5-child"></div>
+                                </div>
+                            </div>
+                        </button>
+                    </div>
                 </div>
-                <div class="child-flex-item4">
-                  {/* <div class="child-flex-item5-child" ></div> */}
-                  <div class="child-flex-item5-child"></div>
-                  <div class="child-flex-item5-child"></div>
-                </div>
-              </div>
-            </button>
-            <button class="parent-flex-item" id="button-5" onClick={this.for5}>
-              <div className="child-container-flex-4">
-                <div class="child-flex-item4">
-                  <div class="child-flex-item4-child"></div>
-                  <div class="child-flex-item4-child"></div>
-                </div>
-                <div class="child-flex-item4">
-                  <div class="child-flex-item5-child"></div>
-                  <div class="child-flex-item5-child"></div>
-                  <div class="child-flex-item5-child"></div>
-                </div>
-              </div>
-            </button>
-            <button class="parent-flex-item" id="button-6" onClick={this.for6}>
-              <div className="child-container-flex-4">
-                <div class="child-flex-item4">
-                  <div class="child-flex-item5-child"></div>
-                  <div class="child-flex-item5-child"></div>
-                  <div class="child-flex-item5-child"></div>
-                </div>
-                <div class="child-flex-item4">
-                  <div class="child-flex-item5-child"></div>
-                  <div class="child-flex-item5-child"></div>
-                  <div class="child-flex-item5-child"></div>
-                </div>
-              </div>
-            </button>
-          </div>
-        </div>
-        <Editor1 num={this.state?.gridnumber} />
-      </div>
-    );
-  }
+                <Editor1 num={this.state?.gridnumber} />
+            </div>
+        );
+    }
 }
 
 export default Flexboxbuttons;
