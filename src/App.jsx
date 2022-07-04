56d2665a0deb5a4e65d6297bb6d81dbab389518f
Utkarshking <Utkarshsinghclash@gmail.com>
2022-07-04 12:26:57 +0530

    state variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 import Editor1 from './components/Editor1';
 // import Resizablelayout from './components/Resizablelayout';
-
+import Flexboxbuttons from './components/Flexboxbuttons';
 function App() {
   
   return (
     <div className="App">
+      <Flexboxbuttons />
      {/* <Resizablelayout/> */}
      {/* <resizableLayoutPackage/> */}
-     <Editor1 />
+     {/* <Editor1 /> */}
     </div>
   );
 }
