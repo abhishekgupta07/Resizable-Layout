a64c30d5cffc56e5d03d1f8e8dfc727ce550e0aa
Utkarsh Singh <Utkarshsinghclash@gmail.com>
2022-07-07 17:03:19 +0530

    fix: local storage managed properly

--- src/components/Threegrid.jsx
+++ src/components/Threegrid.jsx
@@ -3,11 +3,11 @@ import SplitPane from "react-split-pane";
 import logo from "./logoo.png";
 import Twogrid from './Twogrid';
 let secondpane = "550px";
-function Threegrid({ option, size, defaultSizepara2, onChangehandler2,minSizeP1,maxSizeP1, defaultSizepara, onChangehandler }) {
+function Threegrid({ option, size, defaultSizepara2, onChangehandler2,minSizeP1,maxSizeP1, defaultSizepara, onChangehandler,size2}) {
     return (
         <div>
 
-            <SplitPane split={option} defaultSize={defaultSizepara2} minSize={minSizeP1} maxSize={maxSizeP1} primary='first' onChange={onChangehandler2} >
+            <SplitPane split={option} defaultSize={defaultSizepara2} minSize={minSizeP1} maxSize={maxSizeP1} primary='first' onChange={onChangehandler2} size={size}>
                 <canvas style={{ width: "100%", height: "100%", backgroundColor: "rgb(61, 34, 78)", }}>layout2 <img src={logo} alt="logo" width="auto" height="auto" /></canvas>
                 {/* <SplitPane split={option} defaultSize={defaultSizepara} minSize={8} maxSize={-5} primary='second' onChange={onChangehandler} >
                     <canvas style={{ width: "auto", height: "100%", backgroundColor: "#484848", }}>layout2 <img src={logo} alt="logo" width="auto" height="auto" /></canvas>
@@ -17,7 +17,7 @@ function Threegrid({ option, size, defaultSizepara2, onChangehandler2,minSizeP1,
                 defaultSizepara={defaultSizepara}
                 onChangehandler={onChangehandler}
                 minSizeG={100}
-                
+                size={size}
                  />
             </SplitPane>
             {/* </canvas> */}
